7d905b3c54af11c7626c98d49866c372dc0cb69d
Sakshi Kabra <sakshikabra2001@gmail.com>
2021-07-02 04:33:05 +0530

    Add procfile and update ports

--- app.js
+++ app.js
@@ -31,4 +31,11 @@ mongoose.connect(
     () => console.log('connected to DB!')
 )
 
-app.listen(3000);
+let port = process.env.PORT;
+if(port == null || port == ""){
+    port = 3000;
+}
+
+app.listen(port, function(){
+    console.log("Server started successfully")
+});
