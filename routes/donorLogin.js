58ba605e666883ca9f14da848689bdbf34e89b27
amdad7 <70467581+amdad7@users.noreply.github.com>
2021-07-23 01:13:59 +0530

    Update donorLogin.js

--- routes/donorLogin.js
+++ routes/donorLogin.js
@@ -35,13 +35,13 @@ module.exports = async (req, res) => {
           phone: donor.phone,
         };
         let token = authentication.createToken(payloadToCreateToken);
-        let jwtOptions = { expiresIn: 60*60*12*1000 ,httpOnly:true, maxAge:60*60*12*1000,Domain:'edopt-v1.herokuapp.com'};
+        let jwtOptions = { expiresIn: 60*60*12*1000 ,httpOnly:true, maxAge:60*60*12*1000};
         
         //return res.status(200).json({ token });
 
         //console.log(req.headers)
         res.cookie('accesstoken',token,jwtOptions)
-        res.cookie('email',email,{maxAge:60*60*7*1000,encode:String,Domain:'edopt-v1.herokuapp.com'});
+        res.cookie('email',email,{maxAge:60*60*7*1000,encode:String});
         res.status(200)
   
         return res.status(200).json({'status':1,'redirect':redirect,'token':token})
